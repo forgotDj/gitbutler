dec5b638c952b122471a67d1ce91e4088d25993b
Mattias Granlund <mtsgrd@gmail.com>
2026-04-27 13:50:17 +0200

    Enable `but remote` in nightlies

--- scripts/release.sh
+++ scripts/release.sh
@@ -198,9 +198,9 @@ else
 	exit 1
 fi
 
-# Enable IRC collaboration feature for dev and nightly, but not release.
+# Enable IRC collaboration and remote features for dev and nightly, but not release.
 if [ "$CHANNEL" != "release" ]; then
-	FEATURES="$FEATURES irc"
+	FEATURES="$FEATURES irc remote"
 fi
 
 # update the version in the tauri release config
