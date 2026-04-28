8410442fdc07c26a7ea4318348f0ce5780a39379
Oliver Joseph Ash <oliverjash@gmail.com>
2026-04-29 00:58:25 +0100

    Change icon

--- apps/lite/ui/src/icons.tsx
+++ apps/lite/ui/src/icons.tsx
@@ -71,20 +71,12 @@ export const ExpandCollapseIcon: FC<{
 }> = ({ isExpanded }) => (
 	<svg aria-hidden="true" width="16" height="16" viewBox="0 0 16 16">
 		<path
-			d="M4 8h8"
+			d={isExpanded ? "M4.75 6.25 8 9.5l3.25-3.25" : "M6.25 4.75 9.5 8l-3.25 3.25"}
+			fill="none"
 			stroke="currentColor"
 			strokeWidth="1.5"
 			strokeLinecap="round"
 			strokeLinejoin="round"
 		/>
-		{!isExpanded && (
-			<path
-				d="M8 4v8"
-				stroke="currentColor"
-				strokeWidth="1.5"
-				strokeLinecap="round"
-				strokeLinejoin="round"
-			/>
-		)}
 	</svg>
 );
