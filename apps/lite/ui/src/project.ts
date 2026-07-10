7f37c46baa99455c7d85e18f71c94209bc6f0633
Sam A. Horvath-Hunt <hello@samhh.com>
2026-07-10 14:06:43 +0100

    Extract

--- apps/lite/ui/src/project.ts
+++ apps/lite/ui/src/project.ts
@@ -1,1 +1,7 @@
-export const lastOpenedProjectKey = "lastProject";
+const lastOpenedProjectKey = "lastProject";
+
+export const readLastOpenedProject = (): string | null =>
+	window.localStorage.getItem(lastOpenedProjectKey);
+
+export const writeLastOpenedProject = (projectId: string): void =>
+	window.localStorage.setItem(lastOpenedProjectKey, projectId);
